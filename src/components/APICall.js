790e17a1e7c35709a8c6c98560ef328ed7cd2c21
Recep Cankaya <recepcnky@outlook.com>
2022-08-27 21:26:16 +0300

    Second commit after Filter.js

--- src/components/APICall.js
+++ src/components/APICall.js
@@ -4,18 +4,34 @@ import { useState, useEffect } from 'react'
 
 function APICall() {
   const [datum, setDatum] = useState([])
+  const [loading, setLoading] = useState(false)
 
   useEffect(() => {
-    axios("https://api.spoonacular.com/food/products/search?query=yogurt&apiKey=84e883fc6e4e41b8b4a031c825a7a917")
-    .then(res => setDatum(res.data.products))
-    .catch(err => console.log(err))
+    const fetchAPI = async () => {
+      setLoading(true)
+      try {
+        const res = await axios("https://api.spoonacular.com/food/products/search?query=yogurt&apiKey=84e883fc6e4e41b8b4a031c825a7a917")
+        setDatum(res.data.products)
+        console.log(datum)
+      }
+      catch(err) {
+        console.log(err)
+      }
+      setLoading(false)
+    }
+    fetchAPI()
   }, [])
+
+  if (loading === true) {
+    return <div>Loading...</div>
+  }
 
   return (
     <div>
-      {datum.map((el, idx) => (
-        <div key={idx}>
-          <p>{el.id}</p>
+      {datum.map((el, id) => (
+        <div key={id} className="items">
+          <img src={el.image} alt={el.title} />
+          <p>{el.title}</p>
         </div>
      ))}
     </div>
--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import "../App.css"
 import APICall from "./APICall.js"
+import Filter from "./Filter.js"
 
 function App() {
   return (
     <div className="App">
       <APICall />
+      <Filter />
     </div>
   )
 }
